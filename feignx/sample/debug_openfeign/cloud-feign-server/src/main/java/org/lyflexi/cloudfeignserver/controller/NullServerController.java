5e66458427b7f08917cf4fae84654fab41f53f06
Halfmoonly <liuyanoutsee@outlook.com>
2025-04-04 00:20:53 +0800

    commit user

--- feignx/sample/debug_openfeign/cloud-feign-server/src/main/java/org/lyflexi/cloudfeignserver/controller/NullServerController.java
+++ feignx/sample/debug_openfeign/cloud-feign-server/src/main/java/org/lyflexi/cloudfeignserver/controller/NullServerController.java
@@ -1,5 +1,6 @@
 package org.lyflexi.cloudfeignserver.controller;
 
+import org.lyflexi.cloudfeignapi.User;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
